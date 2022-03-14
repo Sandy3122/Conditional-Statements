7012e9ad0840b2cda442fc2621baf5b37cc2628f
Sandeep <seeram@gmail.com>
2022-03-15 01:12:34 +0530

    first commit

---
+++ 29-11-2021/script.js
@@ -0,0 +1,11 @@
+$("#bt1").click(function(){
+	var value=$("#a").innerHTML='Please Enter Your Name'
+
+	if(value.length>=5)
+	{alert("Okay")}
+
+	var value=$("#b").val()
+	else if(value.length>=8){alert("Okay")}
+
+	else{alert("Not Okay")}
+})
